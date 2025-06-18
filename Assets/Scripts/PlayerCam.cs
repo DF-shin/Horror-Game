877f6438d0aeda6f08f6c805309fe80de69dd883
DF-shin <declanschindler0929@live.com>
2025-06-18 09:50:20 -0400

    Changed Camera for Hero
    Removed HeroController script and created separate scripts of camera movement

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+
+    public float sensX;
+    public float sensY;
+
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    void Start()
+    {
+
+        //Hides and locks the cursor to the middle of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+
+        // get mouse inputs
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate camera and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = quaternion.Euler(0, yRotation, 0);
+    }
+}
